c4b742008235a414294e3906eb9ff1a007e829df
Praveen Rajendran <praveenraja9333@gmail.com>
2023-01-08 16:55:23 +0000

    Commit_on_08_01_2023: Incremental development little functional

---
+++ src/main/java/org/vrp/utils/common/Pair.java
@@ -0,0 +1,31 @@
+package org.vrp.utils.common;
+
+public class Pair<K,V> {
+    K key;
+    V value;
+
+    public Pair() {
+
+    }
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+}
