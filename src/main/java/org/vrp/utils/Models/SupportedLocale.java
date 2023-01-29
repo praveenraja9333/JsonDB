a85018ec46a5d0f4c0e2c87303dc1d3b335fe15d
Praveen Rajendran <praveenraja9333@gmail.com>
2023-01-29 20:58:49 +0000

    Commit_on_29_01_2023: Incremental development little functional_more cleanup needed

---
+++ src/main/java/org/vrp/utils/Models/SupportedLocale.java
@@ -0,0 +1,25 @@
+package org.vrp.utils.Models;
+
+public class SupportedLocale {
+    String localeIdentifier;
+    int languageIdentifier;
+
+    public int getLanguageIdentifier() {
+        return languageIdentifier;
+    }
+
+    public void setLanguageIdentifier(int languageIdentifier) {
+        this.languageIdentifier = languageIdentifier;
+    }
+
+
+
+
+    public String getLocaleIdentifier() {
+        return localeIdentifier;
+    }
+
+    public void setLocaleIdentifier(String localeIdentifier) {
+        this.localeIdentifier = localeIdentifier;
+    }
+}
---
+++ src/main/java/org/vrp/utils/exceptions/MandatoryMemberMissingException.java
@@ -0,0 +1,7 @@
+package org.vrp.utils.exceptions;
+
+public class MandatoryMemberMissingException extends RuntimeException {
+    public MandatoryMemberMissingException(String message){
+        super(message);
+    }
+}
